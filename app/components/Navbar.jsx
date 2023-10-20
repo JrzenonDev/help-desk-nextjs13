929e87185bec0613718dcf233d32c09e9d874c18
JrzenonDev <jrobertoonb@gmail.com>
2023-10-20 18:17:06 -0300

    feat: adding logo in navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Logo from "../../public/logo02.png";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="Help desk logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Helpdesk</h1>
       <Link href="/">Home</Link>
       <Link href="/tickets">Tickets</Link>
