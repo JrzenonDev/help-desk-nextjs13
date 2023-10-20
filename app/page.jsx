44b330f0b0fc6afa3c08711e70eade4ad9f9d159
JrzenonDev <jrobertoonb@gmail.com>
2023-10-20 11:06:49 -0300

    feat: create initial structure home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,41 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h2>Dashboard</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam unde
+        deserunt, rem voluptates ipsum laborum ad veritatis, nulla est atque
+        dolorem, dicta nemo. Obcaecati atque illo tempore maxime, est quidem!
+      </p>
+
+      <div className="flex justify-center my-8">
+        <Link href="tickets">
+          <button className="btn-primary">View Tickets</button>
+        </Link>
+      </div>
+
+      <h2>Company Updates</h2>
+
+      <div className="card">
+        <h3>New member of the web dev team...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          facilis aliquid quam odio aspernatur perferendis voluptatem, ex
+          cupiditate quasi impedit accusamus odit est, ab beatae molestias
+          asperiores? Iste, vel unde?
+        </p>
+      </div>
+      <div className="card">
+        <h3>New website live!</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          facilis aliquid quam odio aspernatur perferendis voluptatem, ex
+          cupiditate quasi impedit accusamus odit est, ab beatae molestias
+          asperiores? Iste, vel unde?
+        </p>
+      </div>
     </main>
   );
 }
